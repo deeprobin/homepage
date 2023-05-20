192272eef93a7257b5b6da248c0a082337c24b70
Robin Lindner <robin@deeprobin.de>
2023-05-20 17:03:48 +0200

    Initial commit

--- src/components/Hero/Hero.tsx
+++
@@ -1,9 +0,0 @@
-import "./style.scss";
-
-export const Hero = () => <section class="hero">
-    <div class="content">
-        <h1>Hello World!<br />Robin Lindner here.</h1>
-        <div>Welcome to my digital realm! I'm a passionate Software Developer and the brains behind Lindner IT. Dive in to explore my world of innovation, creativity and relentless pursuit of technological excellence.</div>
-    </div>
-    <img class="avatar" alt="Robin Lindner" src="/images/portrait_comic.svg" width="520" height="735" />
-</section>
